845f2eadee9dcbd5f04c1deddaee2b7eb33c3e9b
Mohamad Saleh <mohamad.saleh@jpl.nasa.gov>
2026-07-02 11:33:48 -0700

    Changed cache size to 300 gbs.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 APP_CONFIG = {
     'CACHE_DIR': os.path.relpath('./cache'),
     'CACHE_FILES': True,
-    'CACHE_MAX_BYTES': int(os.environ.get('CACHE_MAX_BYTES', 10 * 1024 ** 3)),  # 10 GB
+    'CACHE_MAX_BYTES': int(os.environ.get('CACHE_MAX_BYTES', 300 * 1024 ** 3)),  # 300 GB
     'CACHE_TTL_HOURS': int(os.environ.get('CACHE_TTL_HOURS', 0)),
     'CACHE_TARGET_RATIO': float(os.environ.get('CACHE_TARGET_RATIO', 0.85)),
     'AVAILABLE_FORMATS': {
